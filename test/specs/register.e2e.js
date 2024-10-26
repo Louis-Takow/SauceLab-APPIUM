1c87d8b002712c204f3b75aaae5a3bc3571fdcb2
Louis-Takow <louistt1995@gmail.com>
2024-10-26 12:56:02 +0100

    first commit

--- test/specs/register.e2e.js
+++ test/specs/register.e2e.js
@@ -2,7 +2,7 @@ const { expect, browser, $ } = require('@wdio/globals');
 
 describe('My register application - Negative, and Postive cases', () => {
     it('should trigger an error for mismatching passwords - Negative Case', async () => {
-        // click 'Register' 
+        // click 'Register' link 
         const registerLink = await $("//android.view.ViewGroup[@content-desc=\"Register\"]/android.widget.TextView");
         await registerLink.click();
 
@@ -10,37 +10,34 @@ describe('My register application - Negative, and Postive cases', () => {
         const password = await $(`//android.widget.EditText[@text='Password']`);
         await password.setValue("Password@123");
 
-        // set mismatching value to Confirm the password field
+        // set a mismatching value to Confirm the password field
         const confirmPassword = await $("//android.view.ViewGroup[@content-desc=\"Register, Already have an account ? \"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[5]/android.widget.EditText");
         await confirmPassword.setValue("Mismatching password");
 
+        // Assert that the error message is visible
         const errorMessage = await $("//android.view.ViewGroup[@content-desc=\"Register, Already have an account ? \"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.widget.TextView[6]");
-
-        // Assert that the element is visible
         await expect(errorMessage).toBeDisplayed();
 
-       // Assert that the element contains the expected text
+       // Assert that the error message contains the expected text
        const elementText = await errorMessage.getText();
        await expect(elementText).toContain("Password does not match!");
     });
 
     it('should Register with valid credentials - Positive case', async () => {
-
-
-        // set value to first EditText (Name field)
+        // set value to Full Name field
         const fullName = await $("//android.view.ViewGroup[@content-desc=\"Register, Already have an account ? \"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.EditText");
         await fullName.setValue("John Deo");
 
-        // set value to second EditText (Phone number)
+        // set value to Phone number field
         const phoneNumber = await $(`//android.widget.EditText[@text='Phone number']`);
         await phoneNumber.setValue("651562948");
 
-        // set value to Confirm the password field
+        // Clear and set value to Confirm the password field
         const confirmPassword = await $("//android.view.ViewGroup[@content-desc=\"Register, Already have an account ? \"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[5]/android.widget.EditText");
         await confirmPassword.clearValue();
         await confirmPassword.setValue("Password@123");
 
-        // // Perform touch actions to scroll
+        // Perform touch actions to scroll
         await driver.performActions([{
             type: 'pointer',
             id: 'finger1',
